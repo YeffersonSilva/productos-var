05e747b72baef7e3abd2c60c7778dfef8970f0fc
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-30 19:35:45 -0300

    chore: add application.properties, test file, gitignore, and maven config files

---
+++ src/main/java/com/example/productos_var/ProductosVarApplication.java
@@ -0,0 +1,13 @@
+package com.example.productos_var;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosVarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosVarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productos_var/ProductosVarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productos_var;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosVarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
